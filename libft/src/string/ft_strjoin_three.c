5d63dc2682329bb6acfe28dbfaeafe3941923527
Joven <194885329+joven-l@users.noreply.github.com>
2026-01-14 10:31:39 +0800

    add strjoin_three

---
+++ libft/src/string/ft_strjoin_three.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin_three.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/14 10:28:22 by joloo             #+#    #+#             */
+/*   Updated: 2026/01/14 10:31:16 by joloo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin_three(char const *s1, char const *s2, char const *s3)
+{
+	size_t	s1len;
+	size_t	s2len;
+	size_t	s3len;
+	char	*result;
+
+	s1len = ft_strlen(s1);
+	s2len = ft_strlen(s2);
+	s3len = ft_strlen(s3);
+	result = malloc(sizeof(char) * (s1len + s2len + s3len + 1));
+	if (result == NULL)
+		return (NULL);
+	ft_strlcpy(result, s1, s1len + 1);
+	ft_strlcat(result, s2, s1len + s2len + 1);
+	ft_strlcat(result, s3, s1len + s2len + s3len + 1);
+	return (result);
+}
