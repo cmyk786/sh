973f591e68dee8fab1599ca3e91faee3a6212fa5
Joven <194885329+joven-l@users.noreply.github.com>
2026-01-15 13:07:33 +0800

    move env list functions to env_list.c

--- src/env/env_list.c
+++ src/env/env_list.c
@@ -1,25 +1,53 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   env_utils.c                                        :+:      :+:    :+:   */
+/*   env_list.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/06 21:26:47 by joloo             #+#    #+#             */
-/*   Updated: 2026/01/15 13:06:23 by joloo            ###   ########.fr       */
+/*   Created: 2026/01/15 13:06:14 by joloo             #+#    #+#             */
+/*   Updated: 2026/01/15 13:07:02 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env_internal.h"
 
-t_env	*find_node(t_env *env, char *key)
+t_env	*env_node_new(char *key, char *value)
 {
+	t_env	*new;
+
+	new = ft_calloc(1, sizeof(t_env));
+	if (new == NULL)
+		return (NULL);
+	new->key = key;
+	new->value = value;
+	return (new);
+}
+
+void	env_add_back(t_env **head, t_env *new)
+{
+	t_env	*temp;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return ;
+	}
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = new;
+}
+
+int	env_size(t_env *env)
+{
+	int	i;
+	
+	i = 0;
 	while (env != NULL)
 	{
-		if ((env->key != NULL && key != NULL)
-			&& ft_strcmp(env->key, key) == 0)
-			return (env);
 		env = env->next;
+		i++;
 	}
-	return (NULL);
+	return (i);
 }
