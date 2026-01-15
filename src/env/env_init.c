ccd5ba4340a9ff7fc5f146541cc17b8b288d0239
Joven <194885329+joven-l@users.noreply.github.com>
2026-01-15 13:02:52 +0800

    add env_to_arr

--- src/env/env_init.c
+++ src/env/env_init.c
@@ -6,12 +6,13 @@
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 21:02:27 by joloo             #+#    #+#             */
-/*   Updated: 2026/01/14 10:05:38 by joloo            ###   ########.fr       */
+/*   Updated: 2026/01/15 13:02:20 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env_internal.h"
 
+// converts char **envp to t_env *env
 t_env	*env_init(char **envp)
 {
 	int		i;
@@ -34,4 +35,29 @@ t_env	*env_init(char **envp)
 		i++;
 	}
 	return (head);
+}
+
+// converts t_env *env to char **envp
+char **env_to_arr(t_env *env)
+{
+	char	**envp;
+	int		size;
+	int		i;
+
+	env = env->next;
+	size = env_size(env);
+	envp = ft_calloc(sizeof(char *), (size + 1));
+	if (envp == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		envp[i] = ft_strjoin_three(env->key, "=", env->value);
+		if (envp[i] == NULL)
+			return (ft_free_str_arr(envp), NULL);
+		env = env->next;
+		i++;
+	}
+	envp[i] = NULL;
+	return (envp);
 }
