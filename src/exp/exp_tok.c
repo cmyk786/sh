cb8bb19b324e8cb4c9e315c64e81e0b3a2737ffe
Joven <194885329+joven-l@users.noreply.github.com>
2026-01-23 12:16:11 +0800

    finish exp_tok

---
+++ src/exp/exp_tok.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exp_tok.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/18 14:01:35 by joloo             #+#    #+#             */
+/*   Updated: 2026/01/22 22:48:14 by joloo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "exp_internal.h"
+
+int	exp_tok(t_exp *exp)
+{
+	int	i;
+
+	i = 0;
+	while (exp->tok.argv[i] != NULL)
+	{
+		exp->tok.in_squote = 0;
+		exp->tok.in_dquote = 0;
+		if (exp_tok_process(&exp->tok, exp->tok.argv[i]) == FAILURE)
+			return (FAILURE);
+		if (add_delimiter(&exp->tok.tokens) == FAILURE)
+			return (FAILURE);
+		i++;
+	}
+	return (SUCCESS);
+}
+
+int	exp_tok_process(t_exp_tok *tok, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		tok->type = detect_type(tok, str, i);
+		if (exp_tok_call(tok, str, &i) == FAILURE)
+			return (FAILURE);
+	}
+	return (SUCCESS);
+}
+
+int	exp_tok_call(t_exp_tok *tok, char *str, int *i)
+{
+	if (tok->type == QUOTES)
+	{
+		if (exp_tok_quotes(tok, str, i) == FAILURE)
+			return (FAILURE);
+	}
+	else if (tok->type == DQUOTE_VAR || tok->type == UNQUOTE_VAR)
+	{
+		if (exp_tok_var(tok, str, i) == FAILURE)
+			return (FAILURE);
+	}
+	else if (tok->type == WORD)
+	{
+		if (exp_tok_word(tok, str, i) == FAILURE)
+			return (FAILURE);
+	}
+	return (SUCCESS);
+}
--- src/exp/exp_tok_word.c
+++ src/exp/exp_tok_word.c
@@ -1,18 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exp.h                                              :+:      :+:    :+:   */
+/*   exp_tok_word.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/14 10:17:43 by joloo             #+#    #+#             */
-/*   Updated: 2026/01/22 20:46:26 by joloo            ###   ########.fr       */
+/*   Created: 2026/01/22 18:20:08 by joloo             #+#    #+#             */
+/*   Updated: 2026/01/22 22:58:09 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EXP_H
-# define EXP_H
+#include "exp_internal.h"
 
-char	**exp(char **argv, t_env *env)
+int	exp_tok_word(t_exp_tok *tok, char *str, int *i)
+{
+	int		j;
+	t_token	*node;
 
-#endif
+	j = 0;
+	while (str[*i + j] != '\0' 
+		&& detect_type(tok, str, *i + j) == WORD)
+	{
+		j++;
+	}
+	node = create_node(str + *i, j, WORD);
+	if (node == NULL)
+		return (FAILURE);
+	tokenadd_back(&tok->tokens, node);
+	*i += j;
+	return (SUCCESS);
+}
