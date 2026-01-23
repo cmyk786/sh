48c486aa3a3107c857bd03a25afe1013ffd07a56
Joven <194885329+joven-l@users.noreply.github.com>
2026-01-23 12:18:57 +0800

    combine token types into 1 file

--- src/exp/exp_tok_type.c
+++ src/exp/exp_tok_type.c
@@ -1,16 +1,51 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exp_tok_quotes.c                                   :+:      :+:    :+:   */
+/*   exp_tok_type.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/21 17:05:56 by joloo             #+#    #+#             */
-/*   Updated: 2026/01/22 22:56:54 by joloo            ###   ########.fr       */
+/*   Created: 2026/01/23 12:17:08 by joloo             #+#    #+#             */
+/*   Updated: 2026/01/23 12:18:17 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exp_internal.h"
+
+int	exp_tok_var(t_exp_tok *tok, char *str, int *i)
+{
+	int	len;
+	t_token	*node;
+
+	len = name_len(str + *i + 1);
+	if (len == 0)
+	{
+		node = create_node("$", 1, WORD);
+		if (node == NULL)
+			return (FAILURE);
+		*i += 1;
+	}
+	else
+	{
+		node = create_node(str + *i, len + 1, tok->type);
+		if (node == NULL)
+			return (FAILURE);
+		*i += len + 1;
+	}
+	tokenadd_back(&tok->tokens, node);
+	return (SUCCESS);
+}
+
+int	add_delimiter(t_token **tokens)
+{
+	t_token	*node;
+
+	node = create_node(NULL, 0, DELIMITER);
+	if (node == NULL)
+		return (FAILURE);
+	tokenadd_back(tokens, node);
+	return (SUCCESS);
+}
 
 // modifies in_squote && in_dquote
 // returns TRUE on modification of quoted state
@@ -52,3 +87,21 @@ int	exp_tok_quotes(t_exp_tok *tok, char *str, int *i)
 	return (SUCCESS);
 }
 
+int	exp_tok_word(t_exp_tok *tok, char *str, int *i)
+{
+	int		j;
+	t_token	*node;
+
+	j = 0;
+	while (str[*i + j] != '\0' 
+		&& detect_type(tok, str, *i + j) == WORD)
+	{
+		j++;
+	}
+	node = create_node(str + *i, j, WORD);
+	if (node == NULL)
+		return (FAILURE);
+	tokenadd_back(&tok->tokens, node);
+	*i += j;
+	return (SUCCESS);
+}
