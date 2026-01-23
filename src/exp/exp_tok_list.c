468d387b6ca2f026f2d572347b985e661629ea8b
Joven <194885329+joven-l@users.noreply.github.com>
2026-01-23 12:18:57 +0800

    combine token types into 1 file

--- src/exp/exp_tok_list.c
+++ src/exp/exp_tok_list.c
@@ -6,22 +6,11 @@
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 16:40:37 by joloo             #+#    #+#             */
-/*   Updated: 2026/01/22 22:33:51 by joloo            ###   ########.fr       */
+/*   Updated: 2026/01/23 12:17:58 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exp_internal.h"
-
-int	add_delimiter(t_token **tokens)
-{
-	t_token	*node;
-
-	node = create_node(NULL, 0, DELIMITER);
-	if (node == NULL)
-		return (FAILURE);
-	tokenadd_back(tokens, node);
-	return (SUCCESS);
-}
 
 t_token	*create_node(char *str, int len, int type)
 {
