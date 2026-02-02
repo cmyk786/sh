37cc8cf819af102892b636a2d72807ebb076a7a0
Joven <194885329+joven-l@users.noreply.github.com>
2026-02-02 14:43:25 +0800

    fix newline position in syntax_error print message

--- src/parsing/syntax_err.c
+++ src/parsing/syntax_err.c
@@ -6,7 +6,7 @@
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 23:11:37 by joloo             #+#    #+#             */
-/*   Updated: 2026/02/02 14:17:05 by joloo            ###   ########.fr       */
+/*   Updated: 2026/02/02 14:43:11 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ void	syntax_err(t_token *tok)
 	if (tok == NULL)
 		printf("error newlwine\n");
 	else
-		printf("syntax error near unexpected token '%s\n'", tok->value);
+		printf("syntax error near unexpected token '%s'\n", tok->value);
 }
