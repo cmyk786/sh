2a3c66d10ad5b525bf17c434cea4427d8088741d
cmyk786 <mine45345@gmail.com>
2026-02-10 22:30:02 +0800

    Add files via upload

---
+++ src/ex/exit.c
@@ -0,0 +1,56 @@
+static int	is_num(char *s)
+{
+	int	i;
+
+	i = 0;
+	if (s[i] == '+' || s[i] == '-')
+		i++;
+	if (!s[i])
+		return (0);
+	while (s[i])
+	{
+		if (!ft_isdigit(s[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static void	exit_clean(t_ast *node, t_env *env, int n)
+{
+	if (node)
+		clean_node(node);
+	if (env)
+		clean(env);
+	rl_clear_history();
+	exit(n);
+}
+
+void	ft_exit(char **a, t_ast *node, t_env *env)
+{
+	int	n;
+
+	if (!a[1])
+	{
+		printf("exit\n");
+		exit_clean(node, env, ecode);
+	}
+	if (!is_num(a[1]))
+	{
+		printf("exit\n");
+		ft_message("exit: ");
+		ft_message(a[1]);
+		ft_message(": numeric argument required\n");
+		exit_clean(node, env, 2);
+	}
+	if (a[2])
+	{
+		printf("exit\n");
+		ft_message("exit: too many arguments\n");
+		ecode = 2;
+		return ;
+	}
+	n = ft_atoi(a[1]);
+	printf("exit\n");
+	exit_clean(node, env, (unsigned char)n);
+}
