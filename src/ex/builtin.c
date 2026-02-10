6de14003545b726e70a777ce0d4d011bcba80674
cmyk786 <mine45345@gmail.com>
2026-02-10 21:37:00 +0800

    Add files via upload

---
+++ src/ex/builtin.c
@@ -0,0 +1,35 @@
+int is_builtin(char *cmd)
+{
+    if (!cmd)
+        return (0);
+    if (!ft_strcmp(cmd, "cd") || !ft_strcmp(cmd, "echo")
+        || !ft_strcmp(cmd, "pwd") || !ft_strcmp(cmd, "export")
+        || !ft_strcmp(cmd, "unset") || !ft_strcmp(cmd, "env")
+        || !ft_strcmp(cmd, "exit"))
+        return (1);
+    return (0);
+}
+
+int builtin(t_ast *node, t_env **env)
+{
+	char	**argv;
+	
+	argv = node->simple_cmd.argv;
+    if (!node || !argv || !argv[0])
+        return (1);
+    if (!ft_strcmp(argv[0], "cd"))
+        return (cd(argv, env));
+    if (!ft_strcmp(argv[0], "echo"))
+        return (echo(argv));
+    if (!ft_strcmp(argv[0], "pwd"))
+        return (ft_pwd());
+    if (!ft_strcmp(argv[0], "export"))
+        return (export(argv, env));
+    if (!ft_strcmp(argv[0], "unset"))
+        return (unset(argv, env));
+    if (!ft_strcmp(argv[0], "env"))
+        return (ft_env(*env));
+    if (!ft_strcmp(argv[0], "exit"))
+        ft_exit(argv, node, *env);
+    return (1);
+}
