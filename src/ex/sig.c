ebfc3a7410b96c65fed87f2d2d83dc6435a5056b
cmyk786 <mine45345@gmail.com>
2026-02-11 18:07:04 +0800

    Add files via upload

---
+++ src/ex/sig.c
@@ -0,0 +1,17 @@
+#include "ex.h"
+
+void	set_sig_child(void)
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+}
+
+void	sig_prompt(int sig)
+{
+	(void)sig;
+	s = 130;
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
