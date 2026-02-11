73f1bc77330fd2f465bb11bb0ccfc56c709348ab
cmyk786 <mine45345@gmail.com>
2026-02-11 21:34:51 +0800

    Update sig.c

--- src/ex/sig.c
+++ src/ex/sig.c
@@ -6,13 +6,7 @@ void	set_sig_child(void)
 	signal(SIGQUIT, SIG_DFL);
 }
 
-void	set_sig(void)
-{
-	signal(SIGINT, sig_prompt);
-    signal(SIGQUIT, SIG_IGN);
-}
-
-void	sig_prompt(int sig)
+static void	sig_prompt(int sig)
 {
 	(void)sig;
 	s = 130;
@@ -20,7 +14,12 @@ void	sig_prompt(int sig)
 	rl_on_new_line();
 	rl_replace_line("", 0);
 	rl_redisplay();
+}
 
+void	set_sig(void)
+{
+	signal(SIGINT, sig_prompt);
+    signal(SIGQUIT, SIG_IGN);
 }
 
 static void sig_handle(int sig)
@@ -34,3 +33,4 @@ void	set_sig_h(void)
 {
 	signal(SIGINT, sig_handle);
 }
+
