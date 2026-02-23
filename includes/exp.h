0a78e7cb97e3db633cd6c70326b3ed2011e06a4c
Joven <194885329+joven-l@users.noreply.github.com>
2026-02-23 23:38:50 +0800

    update expand_redir prototype

--- includes/exp.h
+++ includes/exp.h
@@ -6,7 +6,7 @@
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 10:17:43 by joloo             #+#    #+#             */
-/*   Updated: 2026/02/20 22:46:12 by joloo            ###   ########.fr       */
+/*   Updated: 2026/02/23 23:38:30 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define EXP_H
 
 char	**exp(char **argv, t_env *env);
-int		expand_redir(char ***res, char *str, t_env *env);
+int		expand_redir(char **str, t_env *env);
 int		exp_hd(t_redir *node,t_env *env);
 
 #endif
