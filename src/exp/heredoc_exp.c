9f8c57e2f5dd9208da183eb0c7faf5ba3bf3b5b3
Joven <194885329+joven-l@users.noreply.github.com>
2026-02-23 19:52:27 +0800

    add static name_len to heredoc_exp.c

--- src/exp/heredoc_exp.c
+++ src/exp/heredoc_exp.c
@@ -6,7 +6,7 @@
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 17:46:57 by joloo             #+#    #+#             */
-/*   Updated: 2026/02/23 19:46:54 by joloo            ###   ########.fr       */
+/*   Updated: 2026/02/23 19:52:13 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,6 +78,27 @@ static int	exp_hd_add_word(char *buffer, char **res, int *i)
 		return (FAILURE);
 	*i += len;
 	return (SUCCESS);
+}
+
+static int	name_len(char *str)
+{
+	int	i;
+
+	if (str[0] == '\0')
+		return (0);
+	if (str[0] == '?')
+		return (1);
+	if (ft_isalpha(str[0]) != 1 && str[0] != '_')
+		return (0);
+	i = 1;
+	while (str[i] != '\0'
+		&& (ft_isalpha(str[i]) == 1
+			|| ft_isdigit(str[i]) == 1
+			|| str[i] == '_'))
+	{
+		i++;
+	}
+	return (i);
 }
 
 static int	exp_hd_add_var(char *buffer, char **res, int *i, t_env *env)
