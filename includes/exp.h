aaa13db747e731d282be2e7eb2e3714ecc824adf
Joven <194885329+joven-l@users.noreply.github.com>
2026-02-27 14:57:28 +0800

    update exp prototypes

--- includes/exp.h
+++ includes/exp.h
@@ -6,14 +6,14 @@
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 10:17:43 by joloo             #+#    #+#             */
-/*   Updated: 2026/02/23 23:38:30 by joloo            ###   ########.fr       */
+/*   Updated: 2026/02/27 12:55:08 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXP_H
 # define EXP_H
 
-char	**exp(char **argv, t_env *env);
+int		expand(char ***argv, t_env *env);
 int		expand_redir(char **str, t_env *env);
 int		exp_hd(t_redir *node,t_env *env);
 
