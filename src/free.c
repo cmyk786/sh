0afab6457ac4ad86b9fca62d1882457f9f6a327d
Joven <194885329+joven-l@users.noreply.github.com>
2026-02-27 14:58:05 +0800

    add free functions

--- src/free.c
+++ src/free.c
@@ -6,21 +6,27 @@
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 22:34:51 by joloo             #+#    #+#             */
-/*   Updated: 2026/02/20 22:36:36 by joloo            ###   ########.fr       */
+/*   Updated: 2026/02/27 12:38:00 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-
+#include "../includes/parsing.h"
+#include "../includes/env.h"
+#include "libft.h"
 // free all including env
 // use when exit
 void	free_all(t_msh *data)
 {
-	(void) data;
+	free_part(data);
+	env_free_all(&data->env);
 }
 
 // free to begin another msh loop
 void	free_part(t_msh *data)
 {
-	(void) data;
+	free_ast(&data->ast);
+	free_tokens(&data->tokens);
+	free(data->input);
+	data->input = NULL;
 }
--- src/parsing/ast_free.c
+++ src/parsing/ast_free.c
@@ -6,7 +6,7 @@
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 21:07:40 by joloo             #+#    #+#             */
-/*   Updated: 2026/02/03 01:30:45 by joloo            ###   ########.fr       */
+/*   Updated: 2026/02/27 14:56:26 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,31 @@
 
 void	free_ast(t_ast **node)
 {
-	(void) node;
+	if (node == NULL || *node == NULL)
+		return ;
+	if ((*node)->type == PIPELINE)
+	{
+		free_ast(&(*node)->control_op.right);
+		free_ast(&(*node)->control_op.left);
+	}
+	else if ((*node)->type == SIMPLE_CMD)
+	{
+		free_redir(&(*node)->simple_cmd.redir);
+		ft_free_str_arr((*node)->simple_cmd.argv);
+	}
+	free(*node);
+	*node = NULL;
 }
 
 void	free_redir(t_redir **node)
 {
-	(void) node;
+	if (node == NULL || *node == NULL)
+		return ;
+	free_redir(&(*node)->next);
+	if ((*node)->type == HERE_DOC)
+		free((*node)->heredoc.line);
+	else
+		free((*node)->word);
+	free(*node);
+	*node = NULL;
 }
