837785bfe5f4cfd4dd33bac9127aa39fcce0347d
Joven <194885329+joven-l@users.noreply.github.com>
2026-02-27 15:23:53 +0800

    fix some norm

--- src/main.c
+++ src/main.c
@@ -6,20 +6,20 @@
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 22:28:22 by joloo             #+#    #+#             */
-/*   Updated: 2026/02/23 19:54:00 by joloo            ###   ########.fr       */
+/*   Updated: 2026/02/27 15:23:43 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-# include "../includes/env.h"
-# include "../includes/tokenize.h"
-# include "../includes/parsing.h"
-# include "../includes/libft.h"
-# include "../includes/debug.h"
+#include "../includes/env.h"
+#include "../includes/tokenize.h"
+#include "../includes/parsing.h"
+#include "../includes/libft.h"
+#include "../includes/debug.h"
 
 void	msh_loop(t_msh *data);
 
-int    main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
 	t_msh	data;
 
@@ -37,7 +37,7 @@ void	msh_loop(t_msh *data)
 	while (1)
 	{
 		// replace with readline
-    	data->input = readline("minishell$");
+		data->input = readline("minishell$");
 		if (!data->input)
 		{
 			printf("exit\n");
@@ -45,20 +45,20 @@ void	msh_loop(t_msh *data)
 		}
 		if (*data->input)
 			add_history(data->input);
-    	data->tokens = tokenize(data->input);
-    	if (data->tokens == NULL)
-      	{
+		data->tokens = tokenize(data->input);
+		if (data->tokens == NULL)
+		{
 			free_part(data);
 			// maybe replace continue with another msh_loop call?
 			continue ;
 		}
-    	print_tokens(data->tokens);
-    	if (parse_ast(data->tokens, &data->ast) == FAILURE)
-     	{
+		print_tokens(data->tokens);
+		if (parse_ast(data->tokens, &data->ast) == FAILURE)
+		{
 			free_part(data);
 			continue ;
 		}
-    	print_ast(data->ast, 0);
+		print_ast(data->ast, 0);
 		// execution
 		free_part(data);
 	}
