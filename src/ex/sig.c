ecf84d2e29fa874b31111d875b23a47609e5d239
cmyk786 <mine45345@gmail.com>
2026-03-01 22:14:04 +0800

    Norm

--- src/ex/sig.c
+++ src/ex/sig.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sig.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/01 02:34:54 by joloo             #+#    #+#             */
+/*   Updated: 2026/03/01 02:34:55 by joloo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ex.h"
 
 void	set_sig_child(void)
@@ -9,7 +21,7 @@ void	set_sig_child(void)
 static void	sig_prompt(int sig)
 {
 	(void)sig;
-	s = 130;
+	g_s = 130;
 	write(1, "\n", 1);
 	rl_on_new_line();
 	rl_replace_line("", 0);
@@ -19,20 +31,17 @@ static void	sig_prompt(int sig)
 void	set_sig(void)
 {
 	signal(SIGINT, sig_prompt);
-    signal(SIGQUIT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
 }
 
-static void sig_handle(int sig)
+static void	sig_handle(int sig)
 {
 	(void)sig;
 	write(2, "\n", 1);
-	s = 130;
+	g_s = 130;
 }
 
 void	set_sig_h(void)
 {
 	signal(SIGINT, sig_handle);
 }
-
-
-
