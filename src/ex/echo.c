d26fe33a38f67a13867f1d31760b400b82d775aa
cmyk786 <mine45345@gmail.com>
2026-03-01 22:03:04 +0800

    Update echo.c

--- src/ex/echo.c
+++ src/ex/echo.c
@@ -1,42 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/01 02:33:05 by joloo             #+#    #+#             */
+/*   Updated: 2026/03/01 02:33:09 by joloo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ex.h"
 
 int	is_n(char *arg)
 {
-    int	i;
+	int	i;
 
 	i = 1;
-    if (arg[0] != '-' || arg[1] != 'n')
-        return (0);
-    while (arg[i])
-    {
-        if (arg[i] != 'n')
-            return (0);
-        i++;
-    }
-    return (1);
+	if (arg[0] != '-' || arg[1] != 'n')
+		return (0);
+	while (arg[i])
+	{
+		if (arg[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 int	echo(char **args)
 {
-    int	i;
-    int	n;
+	int	i;
+	int	n;
 
 	i = 1;
 	n = 1;
-    while (args[i] && is_n(args[i]))
-    {
-        n = 0;
-        i++;
-    }
-    while (args[i])
-    {
-        ft_putstr_fd(args[i], STDOUT_FILENO);
-        if (args[i + 1])
-            ft_putchar_fd(' ', STDOUT_FILENO);
-        i++;
-    }
-    if (n)
-        ft_putchar_fd('\n', STDOUT_FILENO);
-    return (0);
-
+	while (args[i] && is_n(args[i]))
+	{
+		n = 0;
+		i++;
+	}
+	while (args[i])
+	{
+		ft_putstr_fd(args[i], STDOUT_FILENO);
+		if (args[i + 1])
+			ft_putchar_fd(' ', STDOUT_FILENO);
+		i++;
+	}
+	if (n)
+		ft_putchar_fd('\n', STDOUT_FILENO);
+	return (0);
 }
