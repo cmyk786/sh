a93a6e2e9dabef657a2feba24581932d7f6ab254
cmyk786 <mine45345@gmail.com>
2026-03-04 21:37:19 +0800

    Update ex.h

--- src/ex/ex.h
+++ src/ex/ex.h
@@ -41,7 +41,7 @@ int		ft_pwd(void);
 int		unset(char **a, t_env **env);
 int		ft_env(t_env *env);
 int		ft_export(char **arg, t_env **env);
-void	ft_exit(char **a, t_ast *node, t_env *env);
+void	ft_exit(char **a, t_ast *node, t_env **env);
 void	set_fd(int in_fd, int out_fd);
 int		env_size(t_env *env);
 t_env	*copy_env_node(t_env *env);
@@ -56,3 +56,4 @@ int		set_env_value(char *key, char *value, t_env **env);
 int		export_var(char *s, t_env **env);
 
 #endif
+
