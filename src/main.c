629d443a0d53cf04c3281e11a98617f234111619
Joven <194885329+joven-l@users.noreply.github.com>
2026-03-04 16:35:06 +0800

    fix syntax error

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 22:28:22 by joloo             #+#    #+#             */
-/*   Updated: 2026/03/04 16:32:21 by joloo            ###   ########.fr       */
+/*   Updated: 2026/03/04 16:34:50 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,15 +44,15 @@ static void	msh_parse(t_msh *data)
 		printf("Tokenizer FAILURE\n");
 		free_loop(data);
 	}
-	if (debug = TRUE)
+	if (debug == TRUE)
 		print_tokens(data->tokens);
 	if (parse_ast(data->tokens, &data->ast) == FAILURE)
 	{
 		printf("Parsing FAILURE\n");
 		free_loop(data);
 	}
-	free_tokens(data->tokens);
-	if (debug = TRUE)
+	free_tokens(&data->tokens);
+	if (debug == TRUE)
 		print_ast(data->ast, 0);
 }
 
